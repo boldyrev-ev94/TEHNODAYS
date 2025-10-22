5bb8694673ff7d64f5c9b51d85c7cc8dcb56e2e6
Boldyrev-ev94 <Boldyrev.ev94@gmail.com>
2025-10-22 20:04:55 +0700

    fix load main.py backend

--- backend/main.py
+++ backend/main.py
@@ -48,5 +48,5 @@ def get_category(idx: int, check: bool = False, etag: str = None):
     return {**cat, "etag": h}
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
