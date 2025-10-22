d9a55fd115e2975e69bef15040c769bd0c681c00
Boldyrev-ev94 <Boldyrev.ev94@gmail.com>
2025-10-22 15:52:47 +0700

    test fix 2

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://79.141.77.117:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://79.141.77.117"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
