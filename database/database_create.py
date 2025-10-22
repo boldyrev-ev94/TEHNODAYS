79966dad8bc33bbf411f975cf54770b726df38ce
Boldyrev-ev94 <Boldyrev.ev94@gmail.com>
2025-10-23 05:06:38 +0700

    fix creater db

--- database/database_create.py
+++ database/database_create.py
@@ -46,7 +46,7 @@ def main():
             cursor.execute(create_users_table("users"))
             cursor.execute(get_table("users"))
             rows = cursor.fetchall()
-            title = [row.key() for row in rows]
+            title = [row[0] for row in rows.description]
             print(title)
             users = [json.dumps(User(*row).__dict__) for row in rows]
             print(users)
