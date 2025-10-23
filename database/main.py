c96581d41b2f57d314ad7728ddf7eedfeb0e4a92
Boldyrev-ev94 <Boldyrev.ev94@gmail.com>
2025-10-24 03:04:32 +0700

    test 20

--- database/main.py
+++ database/main.py
@@ -59,13 +59,16 @@ WHERE categories.id = {id}
             value = ""
             toplist = []
             for user in data:
-                print(user)
-                # if user['property'] == "value":
-                #     value = int(user['value'])
-                # if user['property'] == "time_up" or user['property'] == "time_down":
-                #     print(user['value'])
-                # minute, sec = map(int, user['value'].split(':'))
-                # value = minute * 60 + sec
+                # print(user)
+                try:
+                    if user['property'] == "value":
+                        value = int(user['value'])
+                    if user['property'] == "time_up" or user['property'] == "time_down":
+                        # print(user['value'])
+                        minute, sec = map(int, user['value'].split(':'))
+                        value = minute * 60 + sec
+                except:
+                    pass
 
                 toplist.append({
                     "name": f"{user['surname']} {user['name']}",
