27eabeaa6af2fd1eb86820c8991c74989b7318da
Boldyrev-ev94 <Boldyrev.ev94@gmail.com>
2025-10-24 01:39:07 +0700

    test 7 query

--- database/main.py
+++ database/main.py
@@ -26,11 +26,11 @@ def get_categorys_dict():
         # INNER JOIN categories ON categories.id = user_category.category_id
         # INNER JOIN users ON users.id = user_category.user_id
         # """
-        sql_query = """
-SELECT categories.name  FROM user_category
+        sql_query = f"""
+SELECT categories.id categories.name FROM user_category
 INNER JOIN categories ON categories.id = user_category.category_id
 INNER JOIN users ON users.id = user_category.user_id
-WHERE categories.name = 'SMS-T'
+WHERE categories.name = '{name_category}'
 """
         cursor.execute(sql_query)
         column_names = [desc[0] for desc in cursor.description]
