cfba8eec1cac72cbf2e257d2990ee054d1033746
Boldyrev-ev94 <Boldyrev.ev94@gmail.com>
2025-10-23 21:57:54 +0700

    fake_data.py init

--- database/fake_data.py
+++ database/fake_data.py
@@ -1,12 +1,55 @@
-def add_user():
+import random
+
+
+def add_user(id):
+    id_technopredki = id
+    name = f"Игрок {id}"
+    surname = f"Фамилия {id}"
+    mng_id = str(random.randint(5))
+    registrator_id = mng_id
+    registrator_name = f"РЕГИСТРАТОР {mng_id}"
+    date_registr = "NOW()"
+    sql = f"""
+    INSERT INTO users (id_technopredki, name, surname, registrator_id, registrator_name, date_registr) 
+    VALUES ({id_technopredki}, {name}, {surname}, {registrator_id}, {registrator_name}, {date_registr}) RETURNING id,
+    """
+    return sql
+
+
+def category_for_user():
     pass
+
+
+def get_table(table_name, cursor):
+    try:
+        # Выполняем запрос
+        cursor.execute(f"SELECT * FROM {table_name}")
+        # Получаем названия столбцов
+        column_names = [desc[0] for desc in cursor.description]
+        # Получаем данные
+        rows = cursor.fetchall()
+        # Преобразуем в список словарей
+        data = [dict(zip(column_names, row)) for row in rows]
+        resaut = {
+            "name_tabel": table_name,
+            "columns": column_names,
+            "data": data
+        }
+        return resaut
+    except Exception as e:
+        print(f"Ошибка при получении данных: {e}")
+        return None
 
 
 def main():
     try:
         db = Database()
         with db.get_cursor() as cursor:
-            cursor.execute(add_users())
+            for i in range(20):
+                cursor.execute(add_users(i))
+            data = get_table("users", cursor)
+            json_table = json.dumps(data, ensure_ascii=False, indent=2)
+            print(json_table)
 
     except Exception as e:
         print(f"Ощибка при добавлении пользователей: {e}")
