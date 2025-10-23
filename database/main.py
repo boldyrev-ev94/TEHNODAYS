1734de9c06a53e46597f9501696603527130ffb9
Boldyrev-ev94 <Boldyrev.ev94@gmail.com>
2025-10-24 02:35:35 +0700

    test 4

--- database/main.py
+++ database/main.py
@@ -58,28 +58,28 @@ WHERE categories.id = {id}
             print(data)
             value = ""
             toplist = []
-            for user in data:
-                print(user)
-                if user['property'] == "value":
-                    value = int(user['value'])
-                else:
-                    minute, sec = map(int, user['value'].split(':'))
-                    value = minute * 60 + sec
+            # for user in data:
+            #     print(user)
+            #     if user['property'] == "value":
+            #         value = int(user['value'])
+            #     else:
+            #         minute, sec = map(int, user['value'].split(':'))
+            #         value = minute * 60 + sec
 
-                toplist.append({
-                    "name": f"{user['surname']} {user['name']}",
-                    'value': value
-                })
-            res_list_users = []
-            if user['property'] == "value":
-                res_list_users = sorted(
-                    toplist, key=lambda x: x['value'], reverse=True)[:15]
-            elif user['property'] == "time_up":
-                res_list_users = sorted(
-                    toplist, key=lambda x: x['value'], reverse=True)[:15]
-            else:
-                res_list_users = sorted(
-                    toplist, key=lambda x: x['value'], reverse=True)[-15:]
+            #     toplist.append({
+            #         "name": f"{user['surname']} {user['name']}",
+            #         'value': value
+            #     })
+            # res_list_users = []
+            # if user['property'] == "value":
+            #     res_list_users = sorted(
+            #         toplist, key=lambda x: x['value'], reverse=True)[:15]
+            # elif user['property'] == "time_up":
+            #     res_list_users = sorted(
+            #         toplist, key=lambda x: x['value'], reverse=True)[:15]
+            # else:
+            #     res_list_users = sorted(
+            #         toplist, key=lambda x: x['value'], reverse=True)[-15:]
 
             resaut = {
                 "id": id,
