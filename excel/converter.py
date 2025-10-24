ec5e5d2c12e7ccd04b92b70724ac6b69de6ad189
boldyrev-ev94 <boldyrev.ev94@gmail.com>
2025-10-24 11:50:19 +0700

    test 33

--- excel/converter.py
+++ excel/converter.py
@@ -5,6 +5,47 @@ import os
 from datetime import datetime
 from openpyxl.cell import cell
 
+CATEGOR_PROPERTY = [
+    ("SMS-T", "value", "main_zone"),
+    ("Карандаш кассета", "time_down", "main_zone"),
+    ("Домашний телефон", "time_down", "main_zone"),
+    ("Словарь без инета", "time_down", "main_zone"),
+    ("Старый комп VS Новый", "value", "main_zone"),
+    ("Железный конструктор", "time_down", "main_zone"),
+    ("Перо VS ручка VS Граф.планшет", "time_down", "main_zone"),
+    ("Перемотать ДВД", "time_down", "main_zone"),
+    ("За рулём", "time_up", "main_zone"),
+    ("НТО", "time_down", "main_zone"),
+    ("Гонки", "time_down", "cyber_zone"),
+    ("Пакман", "value", "cyber_zone"),
+    ("Fruit ninja", "value", "cyber_zone"),
+    ("Тетрис", "value", "cyber_zone")
+]
+
+
+CATEGORY = [
+    {"rank": 1, "name": "SMS-T", "category": "рекорд",
+        "score": 0, "user_name": ""},
+    {"rank": 2, "name": "Карандаш кассета",
+        "category": "время (наименьшее)", "score": 0, "user_name": ""},
+    {"rank": 3, "name": "Домашний телефон",
+        "category": "время (наименьшее)", "score": 0, "user_name": ""},
+    {"rank": 4, "name": "Словарь без инета",
+        "category": "время (наименьшее)", "score": 0, "user_name": ""},
+    {"rank": 5, "name": "Старый комп VS Новый",
+        "category": "рекорд", "score": 0, "user_name": ""},
+    {"rank": 6, "name": "Железный конструктор",
+        "category": "время (наименьшее)", "score": 0, "user_name": ""},
+    {"rank": 7, "name": "Перо VS ручка VS Граф.планшет",
+        "category": "время (наименьшее)", "score": 0, "user_name": ""},
+    {"rank": 8, "name": "Перемотать ДВД",
+        "category": "время (наименьшее)", "score": 0, "user_name": ""},
+    {"rank": 9, "name": "За рулём",
+        "category": "время (наибольшее)", "score": 0, "user_name": ""},
+    {"rank": 10, "name": "НТО",
+        "category": "время (наименьшее)", "score": 0, "user_name": ""},
+]
+
 def excel_to_json(excel_file, json_file):
     try:
         # Проверяем существование файла
@@ -58,7 +99,36 @@ def excel_to_json(excel_file, json_file):
         print(f"Ошибка: {fnf_error}")
     except Exception as e:
         print(f"Произошла ошибка: {str(e)}")
+
+
+
+def categoryes(json_file, json_category):
+    try:
+        res = []
+        with open(json_file, 'r', encoding='utf-8') as f:
+            print("start open")
+            # print(json.dumps(f, ensure_ascii=False, indent=2))
+            for categ in CATEGOR_PROPERTY:
+                # list_categories = []
+                # for item in f:
+                #     cate = {
+                #         "name": f"{f['Фамилия']} {f['Имя']}",
+                #         "id": f["Код учатника"],
+                #         "value": f[categ[0]]                        
+                #     }
+                #     list_categories.append(cate)
+                print(categ)
+            res.append(list_categories)
+        print(res)
+            
         
+    except FileNotFoundError as fnf_error:
+        print(f"Ошибка: {fnf_error}")
+    except Exception as e:
+        print(f"Произошла ошибка: {str(e)}")
+        
+        
+    
         
 # Пример использования
 if __name__ == "__main__":
@@ -70,4 +140,5 @@ if __name__ == "__main__":
     print(f"Текущий рабочий каталог: {os.getcwd()}")
     
     excel_to_json(excel_file, json_file)
+    categoryes(json_file, "")
 
